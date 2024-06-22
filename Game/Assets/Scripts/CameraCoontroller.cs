7a1ba25deb55977b0d9e499bbc8edbfe2b512441
Alina <95280574+ens-tk@users.noreply.github.com>
2024-06-22 20:33:14 +0700

    Revert "Ali"

--- Game/Assets/Scripts/CameraCoontroller.cs
+++ Game/Assets/Scripts/CameraCoontroller.cs
@@ -4,23 +4,15 @@ using UnityEngine;
 
 public class CameraCoontroller : MonoBehaviour
 {
-    public float sensitivity = 2.0f; // Чувствительность мыши
-    public float maxYAngle = 90.0f; // Максимальный угол вращения по вертикали
-
-    private float rotationX = 0.0f;
-
-    private void Update()
+    // Start is called before the first frame update
+    void Start()
     {
-        // Получаем ввод от мыши
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
+        
+    }
 
-        // Вращаем персонажа в горизонтальной плоскости
-        transform.parent.Rotate(Vector3.up * mouseX * sensitivity);
-
-        // Вращаем камеру в вертикальной плоскости
-        rotationX -= mouseY * sensitivity;
-        rotationX = Mathf.Clamp(rotationX, -maxYAngle, maxYAngle);
-        transform.localRotation = Quaternion.Euler(rotationX, 0.0f, 0.0f);
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
