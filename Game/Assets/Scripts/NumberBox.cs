73e87ecab5ee42bea78e762c124e7507c7fc6b1a
bdvzt <144367629+bdvzt@users.noreply.github.com>
2024-06-25 16:40:23 +0700

    пытаюсь пятнашки

---
+++ Game/Assets/Scripts/NumberBox.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberBox : MonoBehaviour
+{
+    public int index = 0;
+    int x = 0;
+    int y = 0;
+
+    private Action<int, int> swapFunc = null;
+
+    public void Init(int i, int j, int index, Sprite sprite, Action<int, int> swapFunc)
+    {
+        this.index = index;
+        this.GetComponent<SpriteRenderer>().sprite = sprite;
+        UpdatePos(i, j);
+        this.swapFunc = swapFunc; 
+
+    }
+
+    public void UpdatePos(int i, int j)
+    {
+        x = i;
+        y = j;
+        this.gameObject.transform.localPosition = new Vector2(i, j);
+    }
+
+    public bool IsEmpty()
+    {
+        return index == 16;
+    }
+
+    //private void OnMouseDown()
+    //{
+    //    if (Input.GetMouseButtonDown(0) && swapFunc!=null)
+    //    {
+    //        swapFunc(x, y);
+
+    //    }
+
+    //}
+    private void OnMouseDown()
+    {
+        if (swapFunc != null)
+        {
+            swapFunc(x, y);
+        }
+    }
+
+}
---
+++ Game/Assets/Scripts/Puzzle.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour
+{
+    public NumberBox boxPrefab;
+
+    public NumberBox[,] boxes = new NumberBox[4,4];
+
+    public Sprite[] sprites;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Init()
+    {
+        int n = 0;
+        for (int y = 3; y >=0;y--)
+            for (int x = 0; x < 4; x++)
+            {
+                NumberBox box = Instantiate(boxPrefab, new Vector2(x, y), Quaternion.identity);
+                box.Init(x, y, n + 1, sprites[n], ClickToSwap);
+                boxes[x, y] = box;
+                n++;
+            }
+
+    }
+
+    void ClickToSwap(int x, int y)
+    {
+        
+    }
+
+    int getDx(int x, int y)
+    {
+        // is right empty
+        if (x < 3 && boxes[x+1, y].IsEmpty())
+        {
+            return 1;
+        }
+        // is left empty
+        if (x > 0 && boxes[x - 1, y].IsEmpty())
+        {
+            return -1;
+        }
+        return 0;
+    }
+
+    int getDy(int x, int y)
+    {
+        // is top empty
+        if (y < 3 && boxes[x, y+1].IsEmpty())
+        {
+            return 1;
+        }
+        // is bottom empty
+        if (y> 0 && boxes[x, y-1].IsEmpty())
+        {
+            return -1;
+        }
+        return 0;
+    }
+}
