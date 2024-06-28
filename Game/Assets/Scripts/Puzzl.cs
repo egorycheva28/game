a8be55bf809266274696905333a3e0c57f24809c
egorycheva28 <egor.masha28@list.ru>
2024-06-28 12:53:00 +0700

    15

--- Game/Assets/Scripts/Puzzl.cs
+++ Game/Assets/Scripts/Puzzl.cs
@@ -6,18 +6,27 @@ public class Puzzl : MonoBehaviour
 {
     public NumberBox boxPrefab;
 
+    [SerializeField] GameObject winPanel;
+
+    [SerializeField] GameObject buttonback;
+
+
     public NumberBox[,] boxes = new NumberBox[4, 4];
 
     public Sprite[] sprites;
 
+    private bool isSolved = false;
+
     void Start()
     {
+        winPanel.SetActive(false);  // Скрываем панель победы при старте
         Init();
-        for (int i = 0; i < 933; i++)
+        for (int i = 0; i < 2; i++)
         {
             Shuffle();
         }
 
+        Debug.Log(IsPuzzleSolved() ? "Puzzle is solved" : "Puzzle is not solved");
     }
 
     void Init()
@@ -29,15 +38,6 @@ public class Puzzl : MonoBehaviour
             {
                 NumberBox box = Instantiate(boxPrefab, new Vector2(x, y), Quaternion.identity);
 
-                //// Проверяем, является ли это последней ячейкой
-                //if (x == 3 && y == 0)
-                //{
-                //    box.Init(x, y, 16, null, ClickToSwap); // Индекс 16 означает пустую ячейку
-                //}
-                //else
-                //{
-                //    box.Init(x, y, n + 1, n < sprites.Length ? sprites[n] : null, ClickToSwap);
-                //}
                 box.Init(x, y, n + 1, sprites[n], ClickToSwap);
 
                 boxes[x, y] = box;
@@ -48,40 +48,53 @@ public class Puzzl : MonoBehaviour
 
     void ClickToSwap(int x, int y)
     {
+        if (isSolved)
+        {
+            return;  // Блокируем движение, если головоломка решена
+        }
+
         int dx = getDx(x, y);
         int dy = getDy(x, y);
 
         Swap(x, y, dx, dy);
+
+        if (IsPuzzleSolved())
+        {
+            Debug.Log("Puzzle is solved");
+            winPanel.SetActive(true);
+            buttonback.SetActive(false);
+            isSolved = true;
+        }
+        else
+        {
+            Debug.Log("Puzzle is NOT solved");
+        }
     }
 
     void Swap(int x, int y, int dx, int dy)
     {
-
-
         if (dx == 0 && dy == 0)
         {
-            return; // Ничего не делаем, если нет доступных для обмена ячеек
+            return;
         }
 
         var from = boxes[x, y];
         var target = boxes[x + dx, y + dy];
-        // swap this 2 boxes
+
         boxes[x, y] = target;
         boxes[x + dx, y + dy] = from;
 
-        //update pos 2 boxes
         from.UpdatePos(x + dx, y + dy);
         target.UpdatePos(x, y);
     }
 
     int getDx(int x, int y)
     {
-        // справа пусто?
         if (x < 3 && boxes[x + 1, y].IsEmpty())
         {
             return 1;
         }
-        // слева пусто?
+
         if (x > 0 && boxes[x - 1, y].IsEmpty())
         {
             return -1;
@@ -91,12 +104,11 @@ public class Puzzl : MonoBehaviour
 
     int getDy(int x, int y)
     {
-        // сверху пусто?
         if (y < 3 && boxes[x, y + 1].IsEmpty())
         {
             return 1;
         }
-        // снизу пусто?
+
         if (y > 0 && boxes[x, y - 1].IsEmpty())
         {
             return -1;
@@ -125,7 +137,6 @@ public class Puzzl : MonoBehaviour
     {
         Vector2 pos = new Vector2();
 
-
         do
         {
             int n = Random.Range(0, 4);
@@ -152,5 +163,31 @@ public class Puzzl : MonoBehaviour
     bool isRepeatMove(Vector2 pos)
     {
         return pos * -1 == lastMove;
+    }
+    bool IsPuzzleSolved()
+    {
+        int count = 1;
+        for (int y = 3; y >= 0; y--)
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                if (y == 0 && x == 3)
+                {
+                    if (!boxes[x, y].IsEmpty())
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (boxes[x, y].index != count)
+                    {
+                        return false;
+                    }
+                    count++;
+                }
+            }
+        }
+        return true;
     }
 }
